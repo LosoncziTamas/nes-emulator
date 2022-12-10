3ac196158c4d6b993f429380ee5209c50430c97d
Tamás Losonczi <losonczi.tamas91@gmail.com>
2022-12-10 21:29:48 +0100

    split declarations to separate files

--- src/hardware/mod.rs
+++ src/hardware/mod.rs
@@ -1,2 +1,4 @@
 pub mod cpu;
 pub mod ppu;
+pub mod ppu_addr_register;
+pub mod ppu_cntrl_register;
--- src/hardware/ppu.rs
+++ src/hardware/ppu.rs
@@ -1,10 +1,12 @@
+use super::ppu_addr_register::AddrRegister;
+use super::ppu_cntrl_register::ControlRegister;
 
 // TODO: remove this when merged with cartridge code
 #[derive(Debug, PartialEq)]
 pub enum Mirroring {
-   Vertical,
-   Horizontal,
-   FourScreen
+    Vertical,
+    Horizontal,
+    FourScreen,
 }
 
 const VRAM_MAX_SIZE: usize = 2048;
@@ -28,9 +30,9 @@ pub struct NesPPU {
     pub internal_data_buf: u8,
 
     pub mirroring: Mirroring,
- }
+}
 
- impl NesPPU {
+impl NesPPU {
     pub fn new(chr_rom: Vec<u8>, mirroring: Mirroring) -> Self {
         NesPPU {
             chr_rom: chr_rom,
@@ -40,7 +42,7 @@ pub struct NesPPU {
             palette_table: [0; PALETTE_TABLE_MAX_SIZE],
             addr: AddrRegister::new(),
             ctrl: ControlRegister::new(),
-            internal_data_buf: 0
+            internal_data_buf: 0,
         }
     }
 
@@ -56,6 +58,7 @@ pub struct NesPPU {
         self.addr.increment(self.ctrl.vram_addr_increment());
     }
 
+    /// read or write access to 0x2007 increments the PPU Address (0x2006)
     fn read_data(&mut self) -> u8 {
         let addr = self.addr.get();
         self.increment_vram_addr();
@@ -71,136 +74,32 @@ pub struct NesPPU {
                 self.internal_data_buf = self.vram[self.mirror_vram_addr(addr) as usize];
                 result
             }
-            0x3000..=0x3eff => panic!("addr space 0x3000..0x3eff is not expected to be used, requested = {} ", addr),
-            0x3f00..=0x3fff => {
-                self.palette_table[(addr - 0x3f00) as usize]
-            }
+            0x3000..=0x3eff => panic!(
+                "addr space 0x3000..0x3eff is not expected to be used, requested = {} ",
+                addr
+            ),
+            0x3f00..=0x3fff => self.palette_table[(addr - 0x3f00) as usize],
             _ => panic!("unexpected access to mirrored space {}", addr),
         }
     }
 
-    // Horizontal:
-   //   [ A ] [ a ]
-   //   [ B ] [ b ]
- 
-   // Vertical:
-   //   [ A ] [ B ]
-   //   [ a ] [ b ]
+    /// Horizontal:
+    ///   [ A ] [ a ]
+    ///   [ B ] [ b ]
 
-   pub fn mirror_vram_addr(&self, addr: u16) -> u16 {
-    let mirrored_vram = addr & 0b10_1111_1111_1111; // mirror down 0x3000-0x3eff to 0x2000 - 0x2eff
-    let vram_index = mirrored_vram - 0x2000; // to vram vector
-    let name_table = vram_index / 0x400; // to the name table index
-    match (&self.mirroring, name_table) {
-        (Mirroring::Vertical, 2) | (Mirroring::Vertical, 3) => vram_index - 0x800,
-        (Mirroring::Horizontal, 2) => vram_index - 0x400,
-        (Mirroring::Horizontal, 1) => vram_index - 0x400,
-        (Mirroring::Horizontal, 3) => vram_index - 0x800,
-        _ => vram_index,
+    /// Vertical:
+    ///   [ A ] [ B ]
+    ///   [ a ] [ b ]
+    pub fn mirror_vram_addr(&self, addr: u16) -> u16 {
+        let mirrored_vram = addr & 0b10_1111_1111_1111; // mirror down 0x3000-0x3eff to 0x2000 - 0x2eff
+        let vram_index = mirrored_vram - 0x2000; // to vram vector
+        let name_table = vram_index / 0x400; // to the name table index
+        match (&self.mirroring, name_table) {
+            (Mirroring::Vertical, 2) | (Mirroring::Vertical, 3) => vram_index - 0x800,
+            (Mirroring::Horizontal, 2) => vram_index - 0x400,
+            (Mirroring::Horizontal, 1) => vram_index - 0x400,
+            (Mirroring::Horizontal, 3) => vram_index - 0x800,
+            _ => vram_index,
+        }
     }
 }
-
- }
-
- const MIRROR_DOWN_ADDRESS_START: u16 = 0x3fff;
-
-
- pub struct AddrRegister {
-    /// high byte first, low byte second
-    value: (u8, u8), 
-    hi_ptr: bool,
- }
-
- impl AddrRegister {
-    pub fn new() -> Self {
-        AddrRegister {
-            value: (0, 0),
-            hi_ptr: true,
-        }
-    }
-    fn set(&mut self, data: u16) {
-        self.value.0 = (data >> 8) as u8;
-        self.value.1 = (data & 0xff) as u8;
-    }
- 
-    pub fn update(&mut self, data: u8) {
-        if self.hi_ptr {
-            self.value.0 = data;
-        } else {
-            self.value.1 = data;
-        }
- 
-        if self.get() > MIRROR_DOWN_ADDRESS_START {
-            self.set(self.get() & 0b11_1111_1111_1111);
-        }
-        self.hi_ptr = !self.hi_ptr;
-    }
- 
-    pub fn increment(&mut self, inc: u8) {
-        let lo = self.value.1;
-        self.value.1 = self.value.1.wrapping_add(inc);
-        if lo > self.value.1 {
-            self.value.0 = self.value.0.wrapping_add(1);
-        }
-        if self.get() > MIRROR_DOWN_ADDRESS_START {
-            self.set(self.get() & 0b11_1111_1111_1111);
-        }
-    }
- 
-    pub fn reset_latch(&mut self) {
-        self.hi_ptr = true;
-    }
- 
-    pub fn get(&self) -> u16 {
-        ((self.value.0 as u16) << 8) | (self.value.1 as u16)
-    }
- }
-
- use bitflags::bitflags;
- bitflags! {
-
-    // 7  bit  0
-    // ---- ----
-    // VPHB SINN
-    // |||| ||||
-    // |||| ||++- Base nametable address
-    // |||| ||    (0 = $2000; 1 = $2400; 2 = $2800; 3 = $2C00)
-    // |||| |+--- VRAM address increment per CPU read/write of PPUDATA
-    // |||| |     (0: add 1, going across; 1: add 32, going down)
-    // |||| +---- Sprite pattern table address for 8x8 sprites
-    // ||||       (0: $0000; 1: $1000; ignored in 8x16 mode)
-    // |||+------ Background pattern table address (0: $0000; 1: $1000)
-    // ||+------- Sprite size (0: 8x8 pixels; 1: 8x16 pixels)
-    // |+-------- PPU master/slave select
-    // |          (0: read backdrop from EXT pins; 1: output color on EXT pins)
-    // +--------- Generate an NMI at the start of the
-    //            vertical blanking interval (0: off; 1: on)
-    pub struct ControlRegister: u8 {
-        const NAMETABLE1              = 0b00000001;
-        const NAMETABLE2              = 0b00000010;
-        const VRAM_ADD_INCREMENT      = 0b00000100;
-        const SPRITE_PATTERN_ADDR     = 0b00001000;
-        const BACKROUND_PATTERN_ADDR  = 0b00010000;
-        const SPRITE_SIZE             = 0b00100000;
-        const MASTER_SLAVE_SELECT     = 0b01000000;
-        const GENERATE_NMI            = 0b10000000;
-    }
- }
-
- impl ControlRegister {
-    pub fn new() -> Self {
-        ControlRegister::from_bits_truncate(0b00000000)
-    }
- 
-    pub fn vram_addr_increment(&self) -> u8 {
-        if !self.contains(ControlRegister::VRAM_ADD_INCREMENT) {
-            1
-        } else {
-            32
-        }
-    }
- 
-    pub fn update(&mut self, data: u8) {
-        self.bits = data;
-    }
- }
