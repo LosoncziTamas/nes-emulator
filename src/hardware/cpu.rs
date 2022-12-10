f4aa2b73b2e83553670d588c4cb2f9baf46deae5
Tamás Losonczi <losonczi.tamas91@gmail.com>
2022-12-10 19:12:57 +0100

    extract flag setting to separate functions

--- src/hardware/cpu.rs
+++ src/hardware/cpu.rs
@@ -5,34 +5,26 @@ use crate::nes_instructions::instruction_list::Codes;
 fn load_accumulator(cpu: &mut CPU, data: u8) {
     cpu.program_counter += 1;
     cpu.register_a = data;
-    println!("{}", data);
-    if cpu.register_a == 0 {
+    update_zero_and_negative_flags(cpu, cpu.register_a);
+}
+
+fn update_zero_and_negative_flags(cpu: &mut CPU, result: u8){
+    if result == 0 {
         cpu.status = cpu.status | 0b0000_0010;
     } else {
         cpu.status = cpu.status & 0b1111_1101;
     }
 
-    if cpu.register_a & 0b1000_000 != 0 {
+    if result & 0b1000_000 != 0 {
         cpu.status = cpu.status | 0b1000_000;
     } else {
-        cpu.status = cpu.status & 0b0111_111;
+        cpu.status = cpu.status & 0b0111_1111;
     }
 }
 
 fn transfer_accumulator_to_x(cpu: &mut CPU) {
     cpu.register_x = cpu.register_a;
-
-    if (cpu.register_x == 0) {
-        cpu.status = cpu.status | 0b0000_0010;
-    } else {
-        cpu.status = cpu.status & 0b1111_1101;
-    }
-
-    if (cpu.register_x & 0b1000_0000 != 0) {
-        cpu.status = cpu.status | 0b1000_0000;
-    } else {
-        cpu.status = cpu.status & 0b0111_1111;
-    }
+    update_zero_and_negative_flags(cpu, cpu.register_x);
 }
 
 fn make_code(u8op: u8) -> Codes {
@@ -67,17 +59,18 @@ impl CPU {
 
         loop {
             let opcode = program[self.program_counter as usize];
-            println!("{}", opcode);
-            let mut data: u8 = 0;
             self.program_counter += 1;
+
+            let mut data: u8 = 0;
             let counter = self.program_counter as usize;
             if (program.len() > counter)
             {
                 data = program[counter];
             }
+
             let op = make_code(opcode);
             match op {
-                Codes::Lda => {load_accumulator(self, data );}
+                Codes::Lda => { load_accumulator(self, data ); }
                 Codes::Brk => { return; }
                 Codes::Tax => { transfer_accumulator_to_x(self); }
                 Codes::Nll => { continue; }
